d6885abfb1222eccb064b186992f2354a5d10229
Habtamu Bogale <abubogale342@gmail.com>
2020-10-31 12:47:52 +0300

    express simple rest api

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 const host = 'localhost';
 const port = 3000;
 
+// app.use((req,res,next) => {
+//     console.log(req.headers);
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('<html><body><h1>This is express server</h1></body></html>');
+// });
 
-app.use((req,res,next) => {
-    console.log(req.headers);
+app.all('/dishes', (req, res,next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is express server</h1></body></html>');
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res,next) => {
+    res.end(`dishes will be sent to your browser`)
+});
+
+app.post('/dishes', (req, res,next) => {
+    res.end(`will add dish ${req.body.name} with details ${req.body.description}`)
+});
+
+app.put('/dishes', (req, res,next) => {
+    res.statusCode = 403
+    res.end('put operation is not supported')
+});
+
+app.delete('/dishes', (req, res,next) => {
+    res.end(`deleting all operations`)
+});
+
+app.get('/dishes/:dishId', (req, res,next) => {
+    res.end(`dishes will be sent to your browser of id ${req.params.dishId}`)
+});
+
+app.post('/dishes/:dishId', (req, res,next) => {
+    res.statusCode = 403
+    res.end(`post operation failed`)
+});
+
+app.put('/dishes/:dishId', (req, res,next) => {
+    res.write(`updating the dish ${req.params.dishId}`)
+    res.end(`will update dish ${req.body.name} with details ${req.body.description}`)
+});
+
+app.delete('/dishes/:dishId', (req, res,next) => {
+    res.end(`deleting dish ${req.params.dishId}`)
 });
 
 const server = http.createServer(app);
